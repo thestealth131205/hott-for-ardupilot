98073d3ded8093a30440232e75495a485996ee64
Dennis <thestealth131205@googlemail.com>
2015-03-22 17:05:36 +0100

    Create HOTT_config.h

---
+++ APM/HOTT_config.h
@@ -0,0 +1,14 @@
+//
+//	HoTT definitions
+//
+#define HOTT_TELEMETRY
+#define HOTT_TELEMETRY_SERIAL_PORT	1
+#define HOTT_SIM_GPS_SENSOR
+#define HOTT_SIM_EAM_SENSOR
+#define HOTT_SIM_VARIO_SENSOR
+#define HOTT_SIM_GAM_SENSOR 
+//#define HOTT_SIM_TEXTMODE
+// Textmode address to simulate
+#define HOTT_SIM_TEXTMODE_ADDRESS	HOTT_TELEMETRY_GPS_SENSOR_ID
+// Battery remaing capacity alarm percent level
+#define HOTT_BATTERY_CAPACITY_REMAINING_PCT_ALARM 20
